8efbb2b391f36b2d805593da83678b75b1cff74d
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-09-28 22:05:47 -0400

    discord bot

---
+++ botfolder/bot.py
@@ -0,0 +1,38 @@
+import discord
+import responses
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = response.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = 'MTAyNDgzMTQxMjc4MTcxOTU5Mg.G1rGNV.dn57BwlG2WqLaEo008jVjcPkLrbrA4ElzFsJEs'
+    client = discord.Client(intents=discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is up and running.')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+        
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+
+
+    client.run(TOKEN)
---
+++ botfolder/dbot.py
@@ -0,0 +1,5 @@
+from pip import main
+import bot
+
+if __name__ == '__main__':
+    bot.run_discord_bot()
