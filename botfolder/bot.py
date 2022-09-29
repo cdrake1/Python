667a061012b13a80721cb2091295defe9ce36db5
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-09-29 16:43:09 -0400

    fixed bot and removed token

--- botfolder/bot.py
+++ botfolder/bot.py
@@ -1,42 +1,43 @@
 import discord
-import responses
-import os
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = response.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+
+msg = ""
 
 def run_discord_bot():
-    TOKEN = 'MTAyNDg2NjA4MjEyMjY0OTY0MQ.GEs7zg.51tUHKlvH4_1mChXXl6gTxYzJMC6D6r_acBjzY'
-    client = discord.Client(intents=discord.Intents.default())
+    #token and connecting through dbot file
+    TOKEN = 'put token here'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
+        #lets us know bot connected to discord
         print(f'{client.user} has connected to discord!')
 
     @client.event
     async def on_message(message):
+        # bot response
+        global msg
         if message.author == client.user:
             return
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-
+        if message.content.startswith('hello'):
+            msg = await message.channel.send('Hello!')
+        elif message.content.startswith('yo'):
+            msg = await message.channel.send('yo')
+        elif message.content.startswith('hey'):
+            msg = await message.channel.send('yo')
+        elif message.content.startswith('whats up'):
+            msg = await message.channel.send('not much')
+        else:
+            msg = await message.channel.send('yare yare daze')
+        
+        # logging
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-
         print(f"{username} said: '{user_message}' ({channel})")
         
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-
-
-
-    client.run(os.getenv('TOKEN'))
+    # authorize bot
+    client.run(TOKEN)
